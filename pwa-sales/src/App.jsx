29389d463433996cd7651da21ef2b98ac9340841
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2025-12-19 10:33:23 +0700

    chagne route

--- pwa-sales/src/App.jsx
+++ pwa-sales/src/App.jsx
@@ -33,9 +33,6 @@ import PurchaseList from "./pages/purchases/PurchaseList";
 import PurchaseAdd from "./pages/purchases/PurchaseAdd";
 import PurchaseEdit from "./pages/purchases/PurchaseEdit";
 
-//Logout 
-
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -45,40 +42,40 @@ export default function App() {
 
         {/* App Layout */}
         <Route element={<AppLayout />}>
-          {/* Default page after login */}
-          {/* <Route path="/" element={<Navigate to="/customers" />} /> */}
+          {/* ✅ IMPORTANT */}
+          <Route index element={<Navigate to="/customers" replace />} />
 
           {/* Customers */}
-          <Route path="/customers" element={<CustomerList />} />
-          <Route path="/customers/add" element={<CustomerAdd />} />
-          <Route path="/customers/:id" element={<CustomerDetails />} />
-          <Route path="/customers/edit/:id" element={<CustomerEdit />} />
+          <Route path="customers" element={<CustomerList />} />
+          <Route path="customers/add" element={<CustomerAdd />} />
+          <Route path="customers/:id" element={<CustomerDetails />} />
+          <Route path="customers/edit/:id" element={<CustomerEdit />} />
 
           {/* Products */}
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/add" element={<ProductAdd />} />
-          <Route path="/products/edit/:id" element={<ProductEdit />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/add" element={<ProductAdd />} />
+          <Route path="products/edit/:id" element={<ProductEdit />} />
 
           {/* Sales */}
-          <Route path="/sales" element={<Sales />} />
-          <Route path="/sales/ProductSale" element={<ProductSale />} />
-          <Route path="/sales/report" element={<SaleReportPage />} />
-          <Route path="/sales/list" element={<SaleList />} />
-          <Route path="/sales/SaleDetails/:id" element={<SaleDetails />} />
+          <Route path="sales" element={<Sales />} />
+          <Route path="sales/ProductSale" element={<ProductSale />} />
+          <Route path="sales/report" element={<SaleReportPage />} />
+          <Route path="sales/list" element={<SaleList />} />
+          <Route path="sales/SaleDetails/:id" element={<SaleDetails />} />
 
           {/* Payments */}
-          <Route path="/payments" element={<PaymentList />} />
-          <Route path="/payments/add" element={<PaymentAdd />} />
-          <Route path="/payments/edit/:id" element={<PaymentEdit />} />
-          <Route path="/payments/add/:id" element={<PaymentAddFC />} />
+          <Route path="payments" element={<PaymentList />} />
+          <Route path="payments/add" element={<PaymentAdd />} />
+          <Route path="payments/edit/:id" element={<PaymentEdit />} />
+          <Route path="payments/add/:id" element={<PaymentAddFC />} />
 
           {/* Purchases */}
-          <Route path="/purchases" element={<PurchaseList />} />
-          <Route path="/purchases/add" element={<PurchaseAdd />} />
-          <Route path="/purchases/edit/:id" element={<PurchaseEdit />} />
+          <Route path="purchases" element={<PurchaseList />} />
+          <Route path="purchases/add" element={<PurchaseAdd />} />
+          <Route path="purchases/edit/:id" element={<PurchaseEdit />} />
 
           {/* Settings */}
-          <Route path="/settings" element={<Settings />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </BrowserRouter>
