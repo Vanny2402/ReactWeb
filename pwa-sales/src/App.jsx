e58e7f3c2ada36b10442d89fdf19394894d74d77
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2025-12-19 10:48:18 +0700

    update protice route

--- pwa-sales/src/App.jsx
+++ pwa-sales/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 
 import Login from "./pages/Login";
 import Settings from "./pages/Settings";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 // Customers
 import CustomerList from "./pages/customers/CustomerList";
@@ -37,45 +38,38 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Login */}
+        {/* Public */}
         <Route path="/" element={<Login />} />
 
-        {/* App Layout */}
-        <Route element={<AppLayout />}>
-          {/* ✅ IMPORTANT */}
-          <Route index element={<Navigate to="/" replace />} />
+        {/* Protected */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="customers" element={<CustomerList />} />
+            <Route path="customers/add" element={<CustomerAdd />} />
+            <Route path="customers/:id" element={<CustomerDetails />} />
+            <Route path="customers/edit/:id" element={<CustomerEdit />} />
 
-          {/* Customers */}
-          <Route path="customers" element={<CustomerList />} />
-          <Route path="customers/add" element={<CustomerAdd />} />
-          <Route path="customers/:id" element={<CustomerDetails />} />
-          <Route path="customers/edit/:id" element={<CustomerEdit />} />
+            <Route path="products" element={<ProductList />} />
+            <Route path="products/add" element={<ProductAdd />} />
+            <Route path="products/edit/:id" element={<ProductEdit />} />
 
-          {/* Products */}
-          <Route path="products" element={<ProductList />} />
-          <Route path="products/add" element={<ProductAdd />} />
-          <Route path="products/edit/:id" element={<ProductEdit />} />
+            <Route path="sales" element={<Sales />} />
+            <Route path="sales/ProductSale" element={<ProductSale />} />
+            <Route path="sales/report" element={<SaleReportPage />} />
+            <Route path="sales/list" element={<SaleList />} />
+            <Route path="sales/SaleDetails/:id" element={<SaleDetails />} />
 
-          {/* Sales */}
-          <Route path="sales" element={<Sales />} />
-          <Route path="sales/ProductSale" element={<ProductSale />} />
-          <Route path="sales/report" element={<SaleReportPage />} />
-          <Route path="sales/list" element={<SaleList />} />
-          <Route path="sales/SaleDetails/:id" element={<SaleDetails />} />
+            <Route path="payments" element={<PaymentList />} />
+            <Route path="payments/add" element={<PaymentAdd />} />
+            <Route path="payments/edit/:id" element={<PaymentEdit />} />
+            <Route path="payments/add/:id" element={<PaymentAddFC />} />
 
-          {/* Payments */}
-          <Route path="payments" element={<PaymentList />} />
-          <Route path="payments/add" element={<PaymentAdd />} />
-          <Route path="payments/edit/:id" element={<PaymentEdit />} />
-          <Route path="payments/add/:id" element={<PaymentAddFC />} />
+            <Route path="purchases" element={<PurchaseList />} />
+            <Route path="purchases/add" element={<PurchaseAdd />} />
+            <Route path="purchases/edit/:id" element={<PurchaseEdit />} />
 
-          {/* Purchases */}
-          <Route path="purchases" element={<PurchaseList />} />
-          <Route path="purchases/add" element={<PurchaseAdd />} />
-          <Route path="purchases/edit/:id" element={<PurchaseEdit />} />
-
-          {/* Settings */}
-          <Route path="settings" element={<Settings />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ pwa-sales/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+// src/routes/ProtectedRoute.jsx
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function ProtectedRoute() {
+  const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+
+  if (!isLoggedIn) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+}
