ac73348c04fe023b3b7b4f4024f43e0ba79322af
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2025-12-24 00:19:28 +0700

    change api

--- pwa-sales/src/api/api.js
+++ pwa-sales/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
   // baseURL: "https://myinvoice-f70e.onrender.com/api", //Malen
-  // baseURL: "https://myinvoice-server.onrender.com/api", //BongSongha
+  baseURL: "https://myinvoice-server.onrender.com/api", //BongSongha
   headers: {
     "Content-Type": "application/json",
   },
